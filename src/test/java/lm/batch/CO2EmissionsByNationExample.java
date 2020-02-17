4cdbdefe306b39423141f94cac617cac2eea1ffe
h4nek <42340131+h4nek@users.noreply.github.com>
2020-02-17 20:03:38 +0100

    Two new examples to test LM (mainly online)

---
+++ src/test/java/lm/batch/CO2EmissionsByNationExample.java
@@ -0,0 +1,103 @@
+package lm.batch;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple5;
+import lm.batch.ExampleOfflineUtilities.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An example trying to fit CO2 emissions by nation data (source: https://datahub.io/core/co2-fossil-by-nation).
+ * We select a subset of 4 nations. The data are non-linear and with multiple records at one time. 
+ * Also bear in mind that the records for different nations might be available starting from completely different years 
+ * (UK is first - industrial revolution; in contrast, some countries haven't existed until recently).
+ */
+public class CO2EmissionsByNationExample {
+    public static final String INPUT_FILE_PATH = "src/test/resources/co2_emissions/fossil-fuel-co2-emissions-by-nation.csv";
+    public static final double LEARNING_RATE = 0.02; //0.000001;
+    public static final String[] selectNations = {"UNITED KINGDOM", "NORWAY", "CZECH REPUBLIC", "CHINA (MAINLAND)"};
+
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        DataSet<Tuple3<Long, String, Double>> dataSet = env.readCsvFile(INPUT_FILE_PATH)
+                .ignoreInvalidLines()
+                .includeFields("1110000000")
+                .types(Long.class, String.class, Double.class)
+                .filter(x -> {for (int i = 0; i < selectNations.length; i++) {
+                    if (x.f1.equals(selectNations[i]))
+                        return true;
+                } return false;});
+        dataSet.printOnTaskManager("DATA"); //TEST
+        
+        DataSet<Tuple2<Long, Tuple3<Long, String, Double>>> indexedDataSet = dataSet.map(new IndicesMapper<>());
+        indexedDataSet.printOnTaskManager("INDEXED DATA");  //TEST
+
+        DataSet<Tuple2<Long, List<Double>>> inputSet = indexedDataSet.map(x -> {
+            List<Double> y = new ArrayList<>(); y.add(x.f1.f0.doubleValue()/500);
+//            y.add(Math.exp(x.f0.doubleValue()/500));    // "replace" x0 with e^x0
+            for (double d : assignNationCode(x.f1.f1)) {
+                y.add(d);
+            }
+            return Tuple2.of(x.f0, y);}).returns(Types.TUPLE(Types.LONG, Types.LIST(Types.DOUBLE)));
+        DataSet<Tuple2<Long, Double>> outputSet = indexedDataSet.map(x -> Tuple2.of(x.f0, x.f1.f2))
+                .returns(Types.TUPLE(Types.LONG, Types.DOUBLE));
+        inputSet.printOnTaskManager("IN");  //TEST
+        outputSet.printOnTaskManager("OUT");    //TEST
+
+        lm.streaming.LinearRegression lr = new lm.streaming.LinearRegression();
+        DataSet<Tuple2<Long, List<Double>>> alphas = lr.fit(inputSet, outputSet, null, LEARNING_RATE, 
+                inputSet.collect().size(), false);
+        alphas.printOnTaskManager("ALPHA"); //TEST
+
+        List<List<Double>> alphaList = alphas.map(x -> x.f1).returns(Types.LIST(Types.DOUBLE)).collect();
+        List<Double> Alpha = alphaList.get(alphaList.size() - 1);
+
+        DataSet<Tuple2<Long, Double>> results = lm.batch.LinearRegression.predict(inputSet, Alpha);
+
+        indexedDataSet.join(results).where(0).equalTo(0)
+                .with((x,y) -> Tuple5.of(x.f0, x.f1.f0, x.f1.f1, x.f1.f2, y.f1))
+                .returns(Types.TUPLE(Types.LONG, Types.LONG, Types.STRING, Types.DOUBLE, Types.DOUBLE))
+                .printOnTaskManager("PREDS");
+
+        /*Compute the MSE for last Alpha*/
+        System.out.println("last Alpha: " + Alpha);
+        DataSet<Double> mse = ExampleOfflineUtilities.computeMSE(results, outputSet);
+        System.out.println(mse.collect().get(alphaList.size() - 1));
+        
+        /*Check the whole trend of MSE*/
+//        ExampleOfflineUtilities.computeMSETrend(alphaList, inputSet, outputSet);
+        
+        /*Attempt to select best Alpha*/
+        List<Double> minAlpha = ExampleOfflineUtilities.selectMinAlpha(alphaList, inputSet, outputSet);
+        System.out.println("min Alpha: " + minAlpha);
+        
+//        DataSet<Tuple2<Long, Double>> minResults = lm.batch.LinearRegression.predict(inputSet, minAlpha);
+//        DataSet<Double> minMse = ExampleOfflineUtilities.computeMSE(minResults, outputSet);
+//        System.out.println(minMse.collect().get(alphaList.size() - 1));
+
+        ExampleOfflineUtilities.plotLRFit(inputSet, outputSet, results, 0);
+    }
+
+    /**
+     * Converting the nation strings into a double array using One-Hot Encoding.
+     * This should ensure that the nations are suitable as an input to the Linear Regression, and that there isn't any 
+     * unwanted dependency (like order) between them.
+     * @param nation
+     * @return
+     */
+    public static double[] assignNationCode(String nation) {
+        double[] oneHotCode = new double[selectNations.length];
+        for (int i = 0; i < selectNations.length; ++i) {
+            oneHotCode[i] = nation.equals(selectNations[i]) ? 1.0 : 0.0;
+        }
+        return oneHotCode;
+    }
+}
---
+++ src/test/java/lm/batch/PM2Point5PollutionInArea.java
@@ -0,0 +1,173 @@
+package lm.batch;
+
+import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.*;
+import org.apache.flink.util.Collector;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * An example trying to fit and predict the PM2.5 pollution (related to air quality) of a selected area. 
+ * The data has been obtained through a query from here: https://www.epa.gov/outdoor-air-quality-data/download-daily-data
+ * (PM2.5; 2019; Seattle-Tacoma-Bellevue, WA (CBSA)).
+ * The data seems to have a periodic fashion and also shows non-linear immediate tendency. 
+ * There might also be a linear overall tendency.
+ */
+public class PM2Point5PollutionInArea {
+    public static final String INPUT_FILE_PATH = "src/test/resources/pm2.5_pollution/transformed_input/PM2.5 for Seattle-Tacoma-Bellevue, WA Transformed.csv";
+    public static final double LEARNING_RATE = 0.001;
+
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+//        env.getConfig().enableObjectReuse();    // seems to have no effect
+
+        DataSet<Tuple2<Long, List<Double>>> dataSet = env.readTextFile(INPUT_FILE_PATH)
+                .map(line -> {
+                    String[] cells = line.split(",");
+                    List<Double> list = null;
+                    try {
+                        list = Arrays.stream(cells).mapToDouble(Double::parseDouble).boxed()
+                                .collect(Collectors.toList());
+                    }
+                    catch (Exception e) {
+                        System.err.println("invalid line: " + line);
+                    }
+                    return list;
+                }).returns(Types.LIST(Types.DOUBLE))
+                .filter(Objects::nonNull) // skip the header (and potentially any other invalid line)
+                .map(new ExampleOfflineUtilities.IndicesMapper<>());
+        dataSet.printOnTaskManager("DATA"); //TEST
+        
+        /*Predicting the average PM2.5*/
+        DataSet<Tuple2<Long, List<Double>>> inputSet = dataSet.map(x -> {
+            List<Double> list = new ArrayList<>();
+            list.add(x.f1.get(0));
+            list.add(100*Math.sin(x.f1.get(0)));    // introduce periodicity
+            list.add(100*Math.exp(Math.sin(x.f1.get(0))));  // non-linear tendency
+            double avg = 0;
+            for (int i = 1; i < x.f1.size(); ++i) { 
+                double val = x.f1.get(i);
+                if (!Double.isNaN(val))
+                    avg += val;
+            }
+            avg /= x.f1.size() - 1;
+            list.add(avg);
+            return Tuple2.of(x.f0, list);
+        }).returns(Types.TUPLE(Types.LONG, Types.LIST(Types.DOUBLE)));
+        inputSet.printOnTaskManager("INPUT");
+        
+        DataSet<Tuple2<Long, Double>> outputSet = dataSet.map(x -> {
+            double avg = 0;
+            for (int i = 1; i < x.f1.size(); ++i) {
+                double val = x.f1.get(i);
+                if (!Double.isNaN(val))
+                    avg += val;
+            }
+            avg /= x.f1.size() - 1;
+            return Tuple2.of(x.f0-1, avg);  // we'll want to predict the next day's mean
+        }).returns(Types.TUPLE(Types.LONG, Types.DOUBLE));
+        outputSet.printOnTaskManager("OUTPUT"); //TEST
+        
+        
+        lm.streaming.LinearRegression lr = new lm.streaming.LinearRegression();
+        DataSet<Tuple2<Long, List<Double>>> alphasAndMSEs = lr.fit(inputSet, outputSet, null, LEARNING_RATE,
+                dataSet.collect().size(), true);
+        DataSet<Double> mse = alphasAndMSEs.filter(x -> x.f0 == -1).map(x -> x.f1.get(0));
+        DataSet<Tuple2<Long, List<Double>>> alphas = alphasAndMSEs.filter(x -> x.f0 != -1);
+        alphas.printOnTaskManager("ALPHA"); //TEST
+
+        List<List<Double>> alphaList = alphas.map(x -> x.f1).returns(Types.LIST(Types.DOUBLE)).collect();
+        List<Double> Alpha = alphaList.get(alphaList.size() - 1);
+        
+        DataSet<Tuple2<Long, Double>> results = lm.batch.LinearRegression.predict(inputSet, Alpha);
+        outputSet.join(results).where(0).equalTo(0)
+                .with((x, y) -> Tuple3.of(x.f0+1, x.f1, y.f1))
+                .returns(Types.TUPLE(Types.LONG, Types.DOUBLE, Types.DOUBLE))
+                .printOnTaskManager("PREDS");
+
+        DataSet<Double> mse2 = ExampleOfflineUtilities.computeMSE(results, outputSet);
+        System.out.println("final MSE: " + mse2.collect().get(alphaList.size() - 1));
+        System.out.println("MSE estimate: " + mse.collect().get(alphaList.size() - 1));
+
+
+        /*Graph the original data & results*/
+        ExampleOfflineUtilities.plotLRFit(inputSet, outputSet, results, 0, 1, "Day", 
+                "PM2.5 Pollution", "PM2.5 Pollution in Seattle", ExampleOfflineUtilities.PlotType.LINE);
+
+//        System.out.println("MSE estimate: " + lr.getMSE(Alpha));
+//        env.execute();
+    }
+
+    /**
+     * Computes the order of the day in the given year (2019).
+     * @param dateString
+     * @return
+     */
+    public static double dateStringToDayNum(String dateString) {
+        DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            Date date = format.parse(dateString);
+            return Math.round((date.getTime() - format.parse("01/01/2019").getTime()) / 86400000.0);    // 86,400,000‬ = ~number of millis in a day
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return -1.0;
+    }
+    
+    public static class ConvertData {
+        public static final String INPUT_FILE_PATH = "D:\\Programy\\BachelorThesis\\Development\\flink-rc\\src\\test\\resources\\pm2.5_pollution\\original_input\\Los Angeles-Long Beach-Anaheim, CA Redux.csv";
+        public static final String TRANSFORMED_INPUT_PATH = "src/test/resources/pm2.5_pollution/transformed_input/PM2.5 for Los Angeles-Long Beach-Anaheim, CA Transformed.csv";
+
+
+        public static void main(String[] args) throws Exception {
+            ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+            env.setParallelism(1);
+
+            /*Convert the original data into a format suitable for LR*/
+            DataSet<Tuple3<String, Integer, Double>> dataSet = env.readCsvFile(INPUT_FILE_PATH)
+                    .ignoreInvalidLines()
+                    .includeFields("111000000000")
+                    .types(String.class, Integer.class, Double.class);
+            dataSet.printOnTaskManager("DATA"); //TEST
+
+            List<Integer> siteIDs = dataSet.distinct(1).map(x -> x.f1).collect();
+
+
+            DataSet<Tuple3<Double, Integer, Double>> dataSetNumeric =
+                    dataSet.map(x -> Tuple3.of(dateStringToDayNum(x.f0), x.f1, x.f2))
+                            .returns(Types.TUPLE(Types.DOUBLE, Types.INT, Types.DOUBLE));
+            DataSet<List<Double>> reducedSet = dataSetNumeric.groupBy(0).reduceGroup(new GroupReduceFunction<Tuple3<Double, Integer, Double>, List<Double>>() {
+                @Override
+                public void reduce(Iterable<Tuple3<Double, Integer, Double>> values, Collector<List<Double>> out) throws Exception {
+                    List<Double> output = new ArrayList<>(Collections.nCopies(siteIDs.size(), Double.NaN));
+                    for (Tuple3<Double, Integer, Double> value : values) {
+                        if (output.isEmpty()) {
+                            output.add(value.f0);
+                        }
+                        int idx = siteIDs.indexOf(value.f1);    // to make sure we insert the value at the right position
+                        output.set(idx, value.f2);
+                    }
+                    out.collect(output);
+                }
+            });
+            reducedSet.printOnTaskManager("Reduced Set");
+            List<String> headers = new ArrayList<>();
+            headers.add("Day");
+            for (int i = 1; i <= siteIDs.size(); i++) {
+                headers.add(siteIDs.get(i-1).toString());
+            }
+            ExampleOfflineUtilities.writeListDataSetToFile(TRANSFORMED_INPUT_PATH, reducedSet.collect(), headers);
+        }
+    }
+}
